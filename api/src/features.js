44abc9988243a7e9ae0154287aa345d6997189f0
Matt Zandstra <zandstra.matt@gmail.com>
2018-09-13 14:06:27 -0600

    simple search api for features

--- api/src/features.js
+++ api/src/features.js
@@ -1,27 +1,42 @@
 
 const SphericalMercator = require('@mapbox/sphericalmercator')
 const merc = new SphericalMercator({ size: 256 })
+const Feature = require('./models/Feature')
 
-function tile2long(x,z) {
-  return (x/Math.pow(2,z)*360-180);
-}
-
-function tile2lat(y,z) {
-  let n=Math.PI-2*Math.PI*y/Math.pow(2,z);
-  return (180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))));
+function boundsToPolygon(bounds) {
+  let poly = []
+  poly.push([bounds[0], bounds[1]])
+  poly.push([bounds[0], bounds[3]])
+  poly.push([bounds[2], bounds[3]])
+  poly.push([bounds[2], bounds[1]])
+  poly.push([bounds[0], bounds[1]])
+  return poly
 }
 
 module.exports = (event, context, callback) => {
-  let bounds = merc.bbox(event.pathParameters.x, event.pathParameters.y, event.pathParameters.z)
+  let tile = event.pathParameters
+  let bounds = merc.bbox(tile.x, tile.y, tile.z)
 
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin' : '*',
-      'Access-Control-Allow-Credentials' : true,
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify({bounds})
+  options = {
+    index: `local-spatial`,
+    project: 'knifecreek',
+    recordType: 'vri',
+    bounds: boundsToPolygon(bounds),
+    zoom: tile.z
+  }
+
+  let request = new Feature(options)
+  request.get().then((data) => {
+    console.log(`Returned ${data.hits.hits.length} features from local-spatial for tile `)
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Credentials' : true,
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({data})
+    })
   })
 
 };
